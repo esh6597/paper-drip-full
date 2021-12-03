a07ecdc5f41eb5a331e17dd3cf520622115b6006
Erin Han <eshan6597@gmail.com>
2021-12-02 18:00:58 -0700

    Client: configure Bootstrap and React Bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById("root")); //Renders the React app at the "root" div in index.html.
