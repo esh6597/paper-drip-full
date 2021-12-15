ef9f403eec2e9120bd8529718de545c427cd25ce
Erin Han <eshan6597@gmail.com>
2021-12-15 13:29:59 -0700

    API: add Pug templates

--- api/app.js
+++ api/app.js
@@ -6,9 +6,10 @@ const path = require('path');
 const createError = require('http-errors');
 const morgan = require('morgan');
 
+//Mongoose for schemas
 const mongoose = require('mongoose');
 
-//Routers
+//Router Imports
 
 
 const app = express();
@@ -25,3 +26,24 @@ app.use(express.json());
 
 //Serves static files from /public directory.
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Use Routers
+
+
+//Catch missing files and send to error handler--same as Express Generator's
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+//Error Handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  //Only provides error in development mode
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  //Render error page; gives generic server error if no error status.
+  res.status(err.status || 500);
+
+  //Uses Pug layout view to render HTML
+  res.render('error');
+});
