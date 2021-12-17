919b65a10494384857d269b7bf1a6972edf81596
Erin Han <eshan6597@gmail.com>
2021-12-17 16:38:37 -0700

    API: update content.js to work with different types of files

--- api/models/content.js
+++ api/models/content.js
@@ -1,8 +1,25 @@
 //SCHEMA FOR ARTICLE BODY
+//Notice how there's no link for article ID. These are automatically
+//  populated into the article page through the article schema's array.
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const contentSchema = new Schema({
-  
+  //This property is to tell the API how to render this.
+  category: { //I wanted to use type here, but thought it'd be confusing.
+    type: String,
+    required: [true, 'Content category required! Use \'text\', \'image\', or \'video\'.']
+  },
+  //These properties are made optional.
+  //ONLY USE ONE; category property only renders the specified type.
+  text: {
+    type: String
+  },
+  video: {
+    type: String
+  },
+  image: {
+    type: String
+  }
 });
