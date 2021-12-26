17af3b4597708bda3a6c98d53fbc01cc92c53c58
Erin Han <eshan6597@gmail.com>
2021-12-26 10:29:25 -0700

    Client: import component files and update structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import React, {Component} from 'react';
-import { BrowserRouter } from 'react-router-dom'; //Provides routing capability for UI component
-
-//Import stylesheet for rest of the app
-import './App.scss';
-
-//Import UI
+import React, { Component } from 'react';
 import UI from './components/UI';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ConfigureStore } from './redux/configureStore';
+import './App.css';
 
-//Simple app.js React component in order to render with store, nothing more. All UI code will be in UI.js.
-class App extends Component{
-  render(){
-    return(
-      <BrowserRouter>
-        <div className="App">
-          <UI />
-        </div>
-      </BrowserRouter>
+const store = ConfigureStore();
+
+class App extends Component {
+
+  render () {
+    return (
+          <Provider store={store}>
+            <BrowserRouter>
+              <div className="App">
+                <UI />
+              </div>
+            </BrowserRouter>
+          </Provider>
     );
   }
 }
--- client/src/components/UI.js
+++ client/src/components/UI.js
@@ -1,22 +1,169 @@
 import React, { Component } from 'react';
 
-class UI extends Component {
+import variables from '../variables.module.scss';
 
-  constructor(props) {
-      super(props);
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Sidebar from 'react-sidebar';
+import { Link } from 'react-router-dom';
 
-      this.state = {
-          sidebarOpen: false
-      }
-  }
+import Toolbar from './Toolbar';
+import Footer from './Footer';
+import Home from './Home';
+import About from './About';
+import Blog from './Blog';
+import Shop from './Shop';
+import Item from './Item';
+import Article from './Article';
 
-  render() {
-    return(
-      <div>
-        <p>Hello, World!</p>
-      </div>
-    );
-  }
+import { fetchArticles, fetchItems, 
+    fetchReviews, fetchComments,
+    updateCart, removeCartItem, emptyCart,
+    postComment } from '../redux/ActionCreators';
+
+import { BsList } from 'react-icons/bs';
+import Button from '@restart/ui/esm/Button';
+
+const mapStateToProps = state => {
+    return {
+        articles: state.articles,
+        items: state.items,
+        comments: state.comments,
+        reviews: state.reviews,
+        cart: state.cart
+    };
 };
 
-export default UI;
+const mapDispatchToProps = {
+    fetchArticles: () => (fetchArticles()),
+    fetchItems: () => (fetchItems()),
+    fetchReviews: () => (fetchReviews()),
+    fetchComments: () => (fetchComments()),
+    updateCart: (itemId, quantity) => (updateCart(itemId, quantity)),
+    removeCartItem: (itemId) => (removeCartItem(itemId)),
+    emptyCart: () => (emptyCart()),
+    postComment: (articleId, author, summary) => (postComment(articleId, author, summary))
+};
+
+class UI extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sidebarOpen: false
+        }
+
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.fetchArticles();
+        this.props.fetchItems();
+        this.props.fetchReviews();
+        this.props.fetchComments();
+    }
+
+    onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+    }
+
+    toggleSidebar() {
+        this.setState({ sidebarOpen: !this.state.sidebarOpen })
+    }
+
+    render() {
+        const HomePage = () => {
+
+            return (
+                <Home 
+                    articles={this.props.articles}
+                    items={this.props.items}
+                />
+            );
+        };
+
+        const ItemWithId = ({match}) => {
+            return(
+                <Item 
+                    mainItem={this.props.items.items.filter(item => item.id === +match.params.itemId)[0]}
+                    mainReviews={this.props.reviews.reviews.filter(review => review.itemId === +match.params.itemId)}
+                    items={this.props.items}
+                    reviews={this.props.reviews}
+                    updateCart={this.props.updateCart}
+                />
+            );
+        };
+
+        const ArticleWithId = ({match}) => {
+            return(
+                <Article 
+                    mainArticle={this.props.articles.articles.filter(article => article.id === +match.params.articleId)[0]}
+                    mainComments={this.props.comments.comments.filter(comment => comment.articleId === +match.params.articleId)}
+                    articles={this.props.articles}
+                    comments={this.props.comments}
+                    postComment={this.props.postComment}
+                />
+            );
+        };
+
+        return (
+            <div>
+                <Toolbar 
+                    items={this.props.items.items}
+                    cart={this.props.cart}
+                    updateCart={this.props.updateCart}
+                    removeCartItem={this.props.removeCartItem}
+                    emptyCart={this.props.emptyCart}
+                />
+                <Sidebar
+                    sidebar={
+                        <div className='sidebar'>
+                            <Link className="nav-link" to="/home">
+                                <p>Home</p>
+                            </Link>
+                            <Link className="nav-link" to="/about">
+                                About
+                            </Link>
+                            <Link className="nav-link" to="/blog">
+                                Blog
+                            </Link>
+                            <Link className="nav-link" to="/shop">
+                                Shop
+                            </Link>
+                        </div>
+                    }
+                    shadow={false}
+                    open={this.state.sidebarOpen}
+                    onSetOpen={this.onSetSidebarOpen}
+                    styles={{
+                        sidebar: { 
+                            background: variables.colorWarm,
+                            padding: 0,
+                            transition: "transform .2s ease-in-out",
+                            WebkitTransition: "-webkit-transform .2s ease-in-out"
+                        } 
+                    }}
+                >
+                    <Button onClick={() => this.toggleSidebar()} className='sidebarButton'>
+                        <BsList style={{fontSize: 32}} />
+                    </Button>
+                    <div className='content'>
+                        <Switch>
+                            <Route path='/home' component={HomePage} />
+                            <Route exact path='/about' render={() => <About />} />
+                            <Route exact path='/blog' render={() => <Blog articles={this.props.articles} />} />
+                            <Route path='/blog/:articleId' component={ArticleWithId} />
+                            <Route exact path='/shop' render={() => <Shop items={this.props.items} />} />
+                            <Route path='/shop/:itemId' component={ItemWithId} />
+                            <Redirect to='/home' />
+                        </Switch>
+                        <Footer />
+                    </div>
+                </Sidebar>
+            </div>
+        );
+    }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UI));
